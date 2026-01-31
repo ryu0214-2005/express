d4fa8c3753199b4b6261578464f49d9380547e0b
Ryunosuke Ando <2301100069vn@cyber-u.ac.ja>
2026-01-31 16:30:23 +0900

    change massage

--- routes/hello.js
+++ routes/hello.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('hello', { title: 'hello Express2' });
+  res.render('hello', { title: 'Hello Express2' });
 });
 
 module.exports = router;
